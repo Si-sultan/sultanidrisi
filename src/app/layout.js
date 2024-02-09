1c0e5615df17926b0ca66c23b27d835263ce9e05
sultanidrisi <isultanjaved@gmail.com>
2024-02-09 22:45:44 +0530

    9-feb

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,16 +15,15 @@ export const metadata = {
   },
 }
 
+
+
 export default function RootLayout({ children }) {
   return (
     
     <html lang="en">
-      <Head>
-        <meta 
-          name="google-site-verification" 
-          content="qx0XqICRZYUksuAMfswDnNtixRxL-WrjA1KSuPdjdXY" 
-        />
-      </Head>
+      <head>
+          <meta name="google-site-verification" content="qx0XqICRZYUksuAMfswDnNtixRxL-WrjA1KSuPdjdXY" />
+      </head>
       <body className={`{inter.className} bg-[rgb(36,36,36)] text-white scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80`}>
         <Header />
         {children}
--- src/app/page.js
+++ src/app/page.js
@@ -14,27 +14,27 @@ export default function Home() {
 
   return (
     <>
-    <div className='snap-mandatory snap-y z-0 relative'>
-          <CursorPoint />
-          <section  id = "hero" className="snap-start">
-            <HeroSection />
-          </section>
-          <section id = 'about' className="padding snap-center">
-            <About />
-          </section>
-          <section id="experience" className="snap-center">
-            <Experience />
-          </section>
-          <section id='skills' className="padding snap-start">
-            <Skills />
-          </section>
-          <section id='projects' className="snap-start">
-             <Projects /> 
-          </section>
-          <section id='contact' className="padding snap-start">
-            <ContactUs />
-          </section>
-  </div>
+      <div className='snap-mandatory snap-y z-0 relative'>
+            <CursorPoint />
+            <section  id = "hero" className="snap-start">
+              <HeroSection />
+            </section>
+            <section id = 'about' className="padding snap-center">
+              <About />
+            </section>
+            <section id="experience" className="snap-center">
+              <Experience />
+            </section>
+            <section id='skills' className="padding snap-start">
+              <Skills />
+            </section>
+            <section id='projects' className="snap-start">
+              <Projects /> 
+            </section>
+            <section id='contact' className="padding snap-start">
+              <ContactUs />
+            </section>
+    </div>
   </>
       )
 }
